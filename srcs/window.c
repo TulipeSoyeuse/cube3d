e09ed5d33efa61022f3043a75d62dbed8ca87efa
Romain Dupeux <rdupeux@made-f0Cr9s2.clusters.42paris.fr>
2024-12-06 16:10:15 +0100

    printf qui traine

--- srcs/window.c
+++ srcs/window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   window.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rdupeux <rdupeux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 10:20:32 by romain            #+#    #+#             */
-/*   Updated: 2024/04/26 11:26:28 by romain           ###   ########.fr       */
+/*   Updated: 2024/12/06 16:09:15 by rdupeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void	cube_exit(t_params *p)
 
 void	loop(t_params *p)
 {
-	printf("loop\n");
 	mlx_key_hook(p->w.mlx_win, &key_hook, p);
 	mlx_hook(p->w.mlx_win, 17, 0, &close_event, p);
 	mlx_loop(p->w.mlx);
